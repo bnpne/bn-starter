8499a09f01dbfcb4bbe5505495b2d20ff9c85170
bnpne <bentppaine@gmail.com>
2021-12-11 10:22:38 -0800

    default pages

--- components/CanvasContainer.js
+++ components/CanvasContainer.js
@@ -2,11 +2,15 @@ import React, { Suspense } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { Preload, ScrollControls, Scroll, useScroll } from '@react-three/drei'
 
-export default function CanvasContainer({ children }) {
+export default function CanvasContainer({
+  children,
+  damping = '4',
+  pages = '1',
+}) {
   return (
     <Canvas gl={{ antialias: false }} dpr={[1, 1.5]}>
       <Suspense fallback={null}>
-        <ScrollControls damping={4} pages={3}>
+        <ScrollControls damping={damping} pages={pages}>
           {children}
         </ScrollControls>
         <Preload />
