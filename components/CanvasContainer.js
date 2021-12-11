1139094331cb6b157f0e51336e6702c84ab241eb
bnpne <bentppaine@gmail.com>
2021-12-11 10:13:24 -0800

    scroll

---
+++ components/CanvasContainer.js
@@ -0,0 +1,16 @@
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Preload, ScrollControls } from '@react-three/drei'
+
+export default function CanvasContainer({ children }) {
+  return (
+    <Canvas gl={{ antialias: false }} dpr={[1, 1.5]}>
+      <Suspense fallback={null}>
+        <ScrollControls damping={4} pages={3}>
+          {children}
+        </ScrollControls>
+        <Preload />
+      </Suspense>
+    </Canvas>
+  )
+}
--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,18 @@
 import MetaTags from './MetaTags'
 import React from 'react'
+import CanvasContainer from './CanvasContainer'
+import { Scroll, useScroll } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
 
 export default function Layout({ children }) {
   return (
     <div>
       <MetaTags title="" desc="" />
-      <main>{children}</main>
+      <main>
+        <CanvasContainer>
+          <Scroll>{children}</Scroll>
+        </CanvasContainer>
+      </main>
     </div>
   )
 }
